2dee264f9670e29ecaccec6146adcfce6a593692
junior <kurokoharolddarivager@gmail.com>
2025-08-27 15:02:27 -0400

    variable assignement

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello World")
+
+x = 5 * 6
+print ("Answer:", x)
+y=8 / 3
+print ("answer", y)
+D=4200000+23090
+print("answer",D)
+M=4200000 -7633
+print("answer",M)
+A=8%3
+print("answer",A)
+B=(8/3)-(2/3)
+print("answer",B)
